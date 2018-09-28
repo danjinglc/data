ee8d26fb6351822a743e8f886b47bc807ee42796
danjinglc <danjinglc@users.noreply.github.com>
2018-09-28 22:36:28 +0800

    ceshi.py

--- ceshi.py
+++ ceshi.py
@@ -1,3 +1,17 @@
 print("hello")
 print("ok")
 print('this is very good!')
+
+
+for letter in 'Python':     # 第一个实例
+   if letter == 'h':
+      continue
+   print ('当前字母 :', letter)
+ 
+var = 10                    # 第二个实例
+while var > 0:              
+   var = var -1
+   if var == 5:
+      continue
+   print ('当前变量值 :', var)
+print "Good bye!"
